afe46f401781bec7b9d36e189123ccad575f622c
Francesco Iacono <sennozeta@gmail.com>
2023-12-04 12:34:20 +0000

    [BACKEND] Routes comments modified

--- backend/src/routes/auth/index.ts
+++ backend/src/routes/auth/index.ts
@@ -3,10 +3,35 @@ import { SpotifyAuthController } from "../../controllers";
 
 const routes = Router();
 
+/**
+ * Spotify authentication routes.
+ * These routes are used to authenticate the user with Spotify.
+ * The user is redirected to the Spotify login page, and then redirected back
+ * to the application after logging in.
+ */
+
+/**
+ * Redirects the user to the Spotify login page.
+ */
+
 routes.get("/login", SpotifyAuthController.redirectToCallbackUrl);
+
+/**
+ * Handles the callback from the Spotify login page.
+ */
+
 routes.get("/callback", SpotifyAuthController.handleCallback);
-routes.get("/refresh", SpotifyAuthController.refreshAccessToken);
+
+/**
+ * Logs the user out.
+ */
+
 routes.get("/logout", SpotifyAuthController.logout);
+
+/**
+ * Checks if the user is authorized.
+ */
+
 routes.get(
   "/authorized",
   SpotifyAuthController.checkAccessToken,

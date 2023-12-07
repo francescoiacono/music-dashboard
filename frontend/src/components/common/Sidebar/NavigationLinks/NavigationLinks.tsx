4eef780b55622cdb41f4f4e91dcdd472d015912b
Francesco Iacono <sennozeta@gmail.com>
2023-12-07 13:43:42 +0000

    [FRONTEND] Sidebar closes when link clicked in small screens

--- frontend/src/components/common/Sidebar/NavigationLinks/NavigationLinks.tsx
+++ frontend/src/components/common/Sidebar/NavigationLinks/NavigationLinks.tsx
@@ -1,28 +1,39 @@
-import { Link } from 'react-router-dom';
-import classes from './NavigationLinks.module.scss';
+import { Link } from "react-router-dom";
+import { useSidebar } from "../../../../providers";
+import classes from "./NavigationLinks.module.scss";
 
 const links = [
   {
-    name: 'Dashboard',
-    path: '/',
+    name: "Dashboard",
+    path: "/",
   },
   {
-    name: 'Top Artists',
-    path: '/top-artists',
+    name: "Top Artists",
+    path: "/top-artists",
   },
   {
-    name: 'Top Tracks',
-    path: '/top-tracks',
+    name: "Top Tracks",
+    path: "/top-tracks",
   },
 ];
 
 export const NavigationLinks = () => {
+  const { isOpen, isLargeScreen, toggleSidebar } = useSidebar();
+
+  const handleClick = () => {
+    if (isOpen && !isLargeScreen) {
+      toggleSidebar();
+    }
+  };
+
   return (
     <nav className={classes.wrapper}>
       <ul className={classes.links}>
         {links.map((link) => (
           <li key={link.name}>
-            <Link to={link.path}>{link.name}</Link>
+            <Link onClick={handleClick} to={link.path}>
+              {link.name}
+            </Link>
           </li>
         ))}
       </ul>

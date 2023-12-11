11264280fdb4af7c205c815d2489920b0671aac3
Francesco Iacono <sennozeta@gmail.com>
2023-12-11 10:14:26 +0000

    [FRONTEND]: Small Fixes on styles and changed useSWR in api hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,21 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {
   ArtistPage,
   NotFoundPage,
   TopArtistsPage,
   TopTracksPage,
-} from './pages';
-import { Dashboard } from './pages';
+} from "./pages";
+import { Dashboard } from "./pages";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
-        <Route path='/top-artists' element={<TopArtistsPage />} />
-        <Route path='/top-tracks' element={<TopTracksPage />} />
-        <Route path='/artist/:id' element={<ArtistPage />} />
-        <Route path='*' element={<NotFoundPage />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/top-artists" element={<TopArtistsPage />} />
+        <Route path="/top-tracks" element={<TopTracksPage />} />
+        <Route path="/artist/:id" element={<ArtistPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
--- frontend/src/hooks/useApiData/index.ts
+++ frontend/src/hooks/useApiData/index.ts
@@ -2,18 +2,16 @@ import { RequestOptions } from "@spotify-dash/types";
 import { resourcesService } from "../../services";
 import useSWR from "swr";
 
-const createFetcher = <T>(url: string, options?: RequestOptions) => {
-  return async (): Promise<T> => {
-    const response = await resourcesService.fetchResource<T>(url, options);
-    return response;
-  };
+const fetcher = async <T>(url: string, options?: RequestOptions) => {
+  return await resourcesService.fetchResource<T>(url, options);
 };
 
 export const useApiResource = <T>(url: string, options?: RequestOptions) => {
-  const fetcher = createFetcher<T>(url, options);
   const serializedOptions = JSON.stringify(options);
   const swrKey = [url, serializedOptions];
-  const { data, error } = useSWR<T, Error>(swrKey, fetcher);
+  const { data, error } = useSWR<T, Error>(swrKey, () =>
+    fetcher<T>(url, options)
+  );
 
   return { data, loading: !error && !data, error };
 };
